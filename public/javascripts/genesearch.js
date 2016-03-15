75dd12810c365a752ebd23661b646e4b0ff70738
mirrormere <ckjohan@gmail.com>
2016-03-15 12:13:48 -0400

    worked out sorting JSON and tabs, so do not need seperate python scripts for each display. wahoo

--- public/javascripts/genesearch.js
+++ public/javascripts/genesearch.js
@@ -45,16 +45,20 @@ $(function () {
 //						$('#tabs1').append(tablehead)
 						var counter = 0
 						$(positions).each(function (index, item) {
+							console.log(item)
 							filteredJSON = dataParsed.filter(function (x) {
 								return x.position = item;
 							})
-							var tabs = '<li><a href="#' + item +'" data-toggle="tab">' + item + '</a></li>'
+							//console.log(filteredJSON)
+							var tabs = '<li><a href="#' + item +'" role="tab" data-toggle="tab">' + item + '</a></li>'
 							$('#positionTab').append(tabs)
+							console.log($('#positionTab'))
 							var panel = '<div class="tab-pane" id="' + item + '">'
 							$('#tabContent').append(panel)
-							var tablehead = '<table class="table"><tr><th>Chromosome</th><th>Sample</th><th>Position</th><th>Reference</th><th>Alt</th><th>Genotype</th><th>SNPEFF Effect</th></tr></thead>'
-							$('#tabContent').append(tablehead)
+							var tablehead = '<table class="table"><tr><th>Chromosome</th><th>Sample</th><th>Position' + item + '</th><th>Reference</th><th>Alt</th><th>Genotype</th><th>SNPEFF Effect</th></tr></thead><tbody id="tbody' + item +'">'
+							$('#' + item).append(tablehead)
 							$(filteredJSON).each(function (i, elem) {
+								console.log(elem.position)
 								var rw = '<tr>'
 								rw += '<td>' + elem.chromosome + '</td>'
 								rw += '<td>' + elem.sample + '</td>'
@@ -64,13 +68,14 @@ $(function () {
 								rw += '<td>' + elem.genotype + '</td>'
 								rw += '<td>'+ elem.SNPEFF_effect + '</td>'
 								rw += '</tr>'
-								rw += '</div>'
-								console.log(rw)
-								$('#tabContent').append(rw);
+								$('#tbody' + item).append(rw)
 							})
 						}) //close dataParsed each
 						$('#position').show()
 					} //close if position
+					if (this.value == 'SNPEFF_effect') {
+						$('#SNPEFF_effect').show()
+					}
 				})
 				console.log("you're in client and length of data is:", data.length)
 //				$("#AJAXresults").html(data);
