197ffdc9fd4855a487ed0ba41b1fc35d0fb60624
mirrormere <ckjohan@gmail.com>
2016-03-16 11:38:47 -0400

    these search options work. wahoo

--- public/javascripts/genesearch.js
+++ public/javascripts/genesearch.js
@@ -21,44 +21,61 @@ $(function () {
 			success: function(data) {
 				var dataParsed = JSON.parse(data)
 				var positions = _.keys(_.countBy(dataParsed, function (x) { return x.position}))
-				console.log(positions)
+				console.log(positions.length)
+				var samples = _.keys(_.countBy(dataParsed, function (x) { return x.sample}))
+				console.log(samples)
+				var effects = _.keys(_.countBy(dataParsed, function (x) { return x.SNPEFF_effect}))
+				console.log(effects)
 				$('#sort_category').on('change', function() {
 					if (this.value == 'sample') {
+						$('#position').hide()
+						$('#test').hide()
+						$(samples).each(function (index, item) {
+							filteredJSON = dataParsed.filter(function (x) {
+								return x.sample = item
+							})//close json filter
+							var drops = '<li><a href="#' + item + '" data-toggle="tab">' + item + '</a></li>'
+							$('#sampleTabs').append(drops)
+							var panel = '<div class="tab-pane fade" id="' + item + '">'
+							$('#sampleContent').append(panel)
+							var tablehead = '<table class="table"><tr><th>Chromosome</th><th>Sample</th><th>Position</th><th>Reference</th><th>Alt</th><th>Genotype</th><th>SNPEFF Effect</th></tr></thead><tbody id="tbody' + item +'">'
+							$('#' + item).append(tablehead)
+							$(dataParsed).each(function (i, elem) {
+								var rw = '<tr>'
+								rw += '<td>' + elem.chromosome + '</td>'
+								rw += '<td>' + elem.sample + '</td>'
+								rw += '<td>' + elem.position + '</td>'
+								rw += '<td>' + elem.reference + '</td>'
+								rw += '<td>' + elem.alternate + '</td>'
+								rw += '<td>' + elem.genotype + '</td>'
+								rw += '<td>'+ elem.SNPEFF_effect + '</td>'
+								rw += '</tr>'
+								$('#tbody'+item).append(rw);
+							}) // close dataParsed.each function
+						}) // close sample.each()
+						$('.btn.btn-default').on('shown.bs.tab', 'a', function (e) {
+        					if (e.relatedTarget) {
+          					$(e.relatedTarget).removeClass('active');
+        					}
+      					}) //close remove dropdown active
 						$('#sample').show()
-						var output = $('#VCFinfo')
-						$(dataParsed).each(function (index, item) {
-							var rw = '<tr>'
-							rw += '<td>' + item.chromosome + '</td>'
-							rw += '<td>' + item.sample + '</td>'
-							rw += '<td>' + item.position + '</td>'
-							rw += '<td>' + item.reference + '</td>'
-							rw += '<td>' + item.alternate + '</td>'
-							rw += '<td>' + item.genotype + '</td>'
-							rw += '<td>'+ item.SNPEFF_effect + '</td>'
-							rw += '</tr>'
-							output.append(rw);
-						}) // close dataParsed.each function
 					} // close if sample
 					if (this.value == 'position') {
 						$('#sample').hide()
-//						var tablehead = '<table class="table"><thead class="thead-default"><tr><th>Chromosome</th><th>Position</th><th>Reference</th><th>Alt</th><th>Genotype</th><th>SNPEFF Effect</th></tr></thead>'
-//						$('#tabs1').append(tablehead)
-						var counter = 0
 						$(positions).each(function (index, item) {
 							console.log(item)
 							filteredJSON = dataParsed.filter(function (x) {
 								return x.position = item;
 							})
-							//console.log(filteredJSON)
-							var tabs = '<li><a href="#' + item +'" role="tab" data-toggle="tab">' + item + '</a></li>'
-							$('#positionTab').append(tabs)
-							console.log($('#positionTab'))
-							var panel = '<div class="tab-pane" id="' + item + '">'
-							$('#tabContent').append(panel)
-							var tablehead = '<table class="table"><tr><th>Chromosome</th><th>Sample</th><th>Position' + item + '</th><th>Reference</th><th>Alt</th><th>Genotype</th><th>SNPEFF Effect</th></tr></thead><tbody id="tbody' + item +'">'
+							var tabs = '<li><a href="#' + item +'" data-toggle="tab">' + item + '</a></li>'
+							$('#positionTabs').append(tabs)
+							//console.log($('#positionTab'))
+							var panel = '<div class="tab-pane fade" id="' + item + '">'
+							$('#positionContent').append(panel)
+							var tablehead = '<table class="table"><tr><th>Chromosome</th><th>Sample</th><th>Position</th><th>Reference</th><th>Alt</th><th>Genotype</th><th>SNPEFF Effect</th></tr></thead><tbody id="tbody' + item +'">'
 							$('#' + item).append(tablehead)
 							$(filteredJSON).each(function (i, elem) {
-								console.log(elem.position)
+								//console.log(elem)
 								var rw = '<tr>'
 								rw += '<td>' + elem.chromosome + '</td>'
 								rw += '<td>' + elem.sample + '</td>'
@@ -74,8 +91,41 @@ $(function () {
 						$('#position').show()
 					} //close if position
 					if (this.value == 'SNPEFF_effect') {
+						$('#sample').hide()
+						$('position').hide()
+						$(effects).each(function (index, item) {
+							console.log(item)
+							filteredJSON = dataParsed.filter(function (x) {
+								return x.SNPEFF_effect = item;
+							})
+							var tabs = '<li><a href="#' + item +'" data-toggle="tab">' + item + '</a></li>'
+							$('#effectTabs').append(tabs)
+							//console.log($('#effectTab'))
+							var panel = '<div class="tab-pane fade" id="' + item + '">'
+							$('#effectContent').append(panel)
+							var tablehead = '<table class="table"><tr><th>Chromosome</th><th>Sample</th><th>Position</th><th>Reference</th><th>Alt</th><th>Genotype</th><th>SNPEFF Effect</th></tr></thead><tbody id="tbody' + item +'">'
+							$('#' + item).append(tablehead)
+							$(filteredJSON).each(function (i, elem) {
+								//console.log(elem)
+								var rw = '<tr>'
+								rw += '<td>' + elem.chromosome + '</td>'
+								rw += '<td>' + elem.sample + '</td>'
+								rw += '<td>' + elem.position + '</td>'
+								rw += '<td>' + elem.reference + '</td>'
+								rw += '<td>' + elem.alternate + '</td>'
+								rw += '<td>' + elem.genotype + '</td>'
+								rw += '<td>'+ elem.SNPEFF_effect + '</td>'
+								rw += '</tr>'
+								$('#tbody' + item).append(rw)
+							})
+						}) //close dataParsed each
 						$('#SNPEFF_effect').show()
-					}
+					} //close if SNPEFF_effect
+					if (this.value == 'test') {
+						$('#sample').hide()
+						$('#position').hide()
+						$('#test').show()
+					} //close test
 				})
 				console.log("you're in client and length of data is:", data.length)
 //				$("#AJAXresults").html(data);
