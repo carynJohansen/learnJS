4e664b28eac2f378b327e678f302cf274c90243b
mirrormere <ckjohan@gmail.com>
2016-03-18 17:19:38 -0400

    a working sql query and table dispplay of that query.

--- public/javascripts/networksearch.js
+++ public/javascripts/networksearch.js
@@ -3,3 +3,19 @@
 ////    client side     ////
 ///////////////////////////
 
+$(function () {
+	$('#queryClick').on('click', function () {
+		$.ajax({
+			url: '/querying',
+			success: function(data) {
+				//var dataParsed = JSON.parse(data)
+				//console.log(dataParsed)
+				$('#testresults').show()
+				$(data).each(function (index, item) {
+					var row = '<tr><td>' + item.reg + '</td><td>' + item.target + '</td></tr>'
+					$('#netTable').append(row)
+				}) // close each
+			} // close success
+		}) // close ajax
+	}) //close on click
+}) // close initial function
