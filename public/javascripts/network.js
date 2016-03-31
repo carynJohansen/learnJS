763cc81ab57a6969fb6325b3414a21041516136d
mirrormere <ckjohan@gmail.com>
2016-03-31 15:34:29 -0400

    network shows SNPEFF filter options. Need to change the color away from red...

--- public/javascripts/network.js
+++ public/javascripts/network.js
@@ -52,20 +52,42 @@ $(function () {
 				textQuery : in_query
 			},
 			success: function(data) {
-				//$('#textQuery').val(data)
-				//console.log(typeof data == 'string')
+				// parse data into a JSON
 				var json = JSON.parse(data)
 				console.log(json)
+				// Get the filter values
+				var snp_arr = []
+				var lr_arr = []
+				$('input[name="snpeff[]"]:checked').each(function() {
+					snp_arr.push($(this).val())
+				})
+				$('input:checked[name="landrace[]"]').each( function () {
+					lr_arr.push($(this).val());
+				})
+				console.log(snp_arr)
+				console.log(lr_arr)
 				$('#testresults').show()
 				$('#cy').show()
-				var nodes = json.group == "nodes"
-				console.log(nodes.length)
 				var nodeARR = []
 				var edgeARR = []
 				var table = ''
 				$(json).each(function (index, item) {
-					nodeARR.push({data : { id : item.netID_regulator , name : item.regulator, snpeff : item.regulator_info.snpeff_unique, positions : item.regulator_info.variant_positions, muts : item.regulator_info.provean_mutations }, group:"nodes",removed:false,selected:false,selectable:true,locked:false,grabbed:false,grabbable:true})
-					nodeARR.push({data : { id : item.netID_target, name : item.target, snpeff : item.target_info.snpeff_unique, positions : item.target_info.variant_positions, muts : item.target_info.provean_mutations }, group:"nodes",removed:false,selected:false,selectable:true,locked:false,grabbed:false,grabbable:true})
+					var reg_selected = false
+					var tar_selected = false
+					var reg_color = '#555'
+					var tar_color = '#555'
+					$(snp_arr).each(function (i, elem) {
+						if ($.inArray(elem, item.regulator_info.snpeff_unique) > 0 ) {
+							reg_selected = true
+							reg_color = '#DC381F'
+						}
+						if ($.inArray(elem, item.target_info.snpeff_unique) > 0) {
+							tar_selected = true
+							tar_color = '#DC381F'
+						} 
+					})
+					nodeARR.push({data : { id : item.netID_target, name : item.target, snpeff : item.target_info.snpeff_unique, selected : tar_selected, color : tar_color, positions : item.target_info.variant_positions, muts : item.target_info.provean_mutations }, group:"nodes",removed:false,selected:false,selectable:true,locked:false,grabbed:false,grabbable:true})
+					nodeARR.push({data : { id : item.netID_regulator , name : item.regulator, snpeff : item.regulator_info.snpeff_unique, selected : tar_selected, color : reg_color, positions : item.regulator_info.variant_positions, muts : item.regulator_info.provean_mutations }, group:"nodes",removed:false,selected:false,selectable:true,locked:false,grabbed:false,grabbable:true})
 					edgeARR.push({data : {source: item.netID_regulator , target: item.netID_target }, group:"edges",removed:false,selected:false,selectable:true,locked:false,grabbed:false,grabbable:true})
 					table += '<tr><td>' + item.regulator + '</td><td>' + item.regulator_info.provean_mutations + '</td>' 
 					if (typeof item.regulator_info.snpeff_unique == 'object') {
@@ -82,20 +104,28 @@ $(function () {
 						table += '<td>' + item.target_info.snpeff_unique + '</td></tr>'
 					}
 				}) // close each
-				console.log("this is nodeARR:", nodeARR)
-				console.log(edgeARR)
+				console.log(nodeARR)
 				$('#netTable').html(table)
 				$('#cy').cytoscape({
 					layout: {
 						name : 'cose-bilkent'
 					},
-					style: [{"selector":"core","style":{"selection-box-color":"#AAD8FF","selection-box-border-color":"#8BB0D0","selection-box-opacity":"0.5"}},{"selector":"node","style":{"width":"mapData(score, 0, 0.006769776522008331, 20, 60)","height":"mapData(score, 0, 0.006769776522008331, 20, 60)","content":"data(name)","font-size":"12px","text-valign":"center","text-halign":"center","background-color":"#555","text-outline-color":"#555","text-outline-width":"2px","color":"#fff","overlay-padding":"6px","z-index":"10"}},{"selector":"node[?attr]","style":{"shape":"rectangle","background-color":"#aaa","text-outline-color":"#aaa","width":"16px","height":"16px","font-size":"6px","z-index":"1"}},{"selector":"node[?query]","style":{"background-clip":"none","background-fit":"contain"}},{"selector":"node:selected","style":{"border-width":"6px","border-color":"#AAD8FF","border-opacity":"0.5","background-color":"#77828C","text-outline-color":"#77828C"}},{"selector":"edge","style":{"curve-style":"haystack","haystack-radius":"0.5","opacity":"0.4","line-color":"#bbb","width":"mapData(weight, 0, 1, 1, 8)","overlay-padding":"3px"}},{"selector":"node.unhighlighted","style":{"opacity":"0.2"}},{"selector":"edge.unhighlighted","style":{"opacity":"0.05"}},{"selector":".highlighted","style":{"z-index":"999999"}},{"selector":"node.highlighted","style":{"border-width":"6px","border-color":"#AAD8FF","border-opacity":"0.5","background-color":"#394855","text-outline-color":"#394855","shadow-blur":"12px","shadow-color":"#000","shadow-opacity":"0.8","shadow-offset-x":"0px","shadow-offset-y":"4px"}},{"selector":"edge.filtered","style":{"opacity":"0"}},{"selector":"edge[group=\"coexp\"]","style":{"line-color":"#d0b7d5"}},{"selector":"edge[group=\"coloc\"]","style":{"line-color":"#a0b3dc"}},{"selector":"edge[group=\"gi\"]","style":{"line-color":"#90e190"}},{"selector":"edge[group=\"path\"]","style":{"line-color":"#9bd8de"}},{"selector":"edge[group=\"pi\"]","style":{"line-color":"#eaa2a2"}},{"selector":"edge[group=\"predict\"]","style":{"line-color":"#f6c384"}},{"selector":"edge[group=\"spd\"]","style":{"line-color":"#dad4a2"}},{"selector":"edge[group=\"spd_attr\"]","style":{"line-color":"#D0D0D0"}},{"selector":"edge[group=\"reg\"]","style":{"line-color":"#D0D0D0"}},{"selector":"edge[group=\"reg_attr\"]","style":{"line-color":"#D0D0D0"}},{"selector":"edge[group=\"user\"]","style":{"line-color":"#f0ec86"}}],
+					style: [
+						{"selector":"core","style":{"selection-box-color":"#AAD8FF","selection-box-border-color":"#8BB0D0","selection-box-opacity":"0.5"}},
+						{"selector":"node","style":{"content":"data(name)","font-size":"12px","text-valign":"center","text-halign":"center","text-outline-width":"2px","color":"#fff","background-color":"data(color)", "overlay-padding":"6px","z-index":"10"}},
+						{"selector":"node:selected","style":{"border-width":"6px","border-color":"#AAD8FF","border-opacity":"0.5","background-color":"#77828C","text-outline-color":"#77828C"}},
+						{"selector":"edge","style":{"curve-style":"haystack","haystack-radius":"0.5","opacity":"0.4","line-color":"#bbb","width":"mapData(weight, 0, 1, 1, 8)","overlay-padding":"3px"}},
+						{"selector":"node.unhighlighted","style":{"opacity":"0.2"}},
+						{"selector":"edge.unhighlighted","style":{"opacity":"0.05"}},
+						{"selector":".highlighted","style":{"z-index":"999999"}},
+						{"selector":"node.highlighted","style":{"border-width":"6px","border-color":"#AAD8FF","border-opacity":"0.5","background-color":"#394855","text-outline-color":"#394855","shadow-blur":"12px","shadow-color":"#000","shadow-opacity":"0.8","shadow-offset-x":"0px","shadow-offset-y":"4px"}},
+						{"selector":"edge.filtered","style":{"opacity":"0"}}],
 					elements: {
 						nodes: nodeARR,
 						edges : edgeARR
 					},
 					minZoom: 0.02,
-					maxZoom: 1,
+					maxZoom: 5,
 					ready: function(){
     					window.cy = this;
     					cy.elements().unselectify();
